7000063929ee414e0e719cedf7668b23384e1f47
Hayato <alfort625@gmail.com>
2017-06-02 15:40:06 +0900

    Change activity to use implicit intent

--- app/src/main/java/jp/ac/titech/itpro/sdl/activityuser/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/activityuser/MainActivity.java
@@ -35,9 +35,9 @@ public class MainActivity extends AppCompatActivity {
                 request = getString(R.string.request_3_text);
                 break;
         }
-        Intent intent = new Intent();
-        intent.setClassName("jp.ac.titech.itpro.sdl.activityprovider",
-                "jp.ac.titech.itpro.sdl.activityprovider.PublicActivity");
+      Intent intent = new Intent("android.intent.action.IMPLICIT");
+//        intent.setClassName("jp.ac.titech.itpro.sdl.activityprovider",
+//                "jp.ac.titech.itpro.sdl.activityprovider.PublicActivity");
         intent.putExtra("request", request);
         startActivityForResult(intent, MYREQCODE);
     }
